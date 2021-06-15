c04635c14733dda813e6e97e8734e57f212588f6
Jkmulinge <83027816+Jkmulinge@users.noreply.github.com>
2021-06-15 09:41:28 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d5eff06f840b45e9"
+  instance_type = "t1.micro"
+  tags = {
+    Name = "CreatedInTerraform"
+  }
+}
