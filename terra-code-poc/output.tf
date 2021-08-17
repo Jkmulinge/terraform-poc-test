cdb3da989f2bf9710ea38e06bff085d49b5b9e4b
Jkmulinge <83027816+Jkmulinge@users.noreply.github.com>
2021-08-17 09:50:36 -0400

    Update output.tf

--- terra-code-poc/output.tf
+++ terra-code-poc/output.tf
@@ -1,3 +1,3 @@
 output "ec2_machines" {
-  value = aws_instance.my-machine.*.arn  # Here * indicates that there are more than one arn as we used count as 4   
+  value = aws_instance.app_server.*.arn  # Here * indicates that there are more than one arn as we used count as 3   
 }
