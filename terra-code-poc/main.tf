f5407603531ddbf680378e9fc7a2b393b5d8dc7d
Jkmulinge <83027816+Jkmulinge@users.noreply.github.com>
2021-08-17 09:41:07 -0400

    Update main.tf

--- terra-code-poc/main.tf
+++ terra-code-poc/main.tf
@@ -1,1 +1,8 @@
-
+resource "aws_instance" "app_server" {
+  count = 3 
+  ami           =  var.ami "ami-0d5eff06f840b45e9"
+  instance_type = var.instance_type "t1.micro"
+  tags = {
+   Name = "Terra-machine-${count.index}"
+  }
+}
